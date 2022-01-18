9b5b8b0412278930911b8eda57ca42a10313e548
Emily Chen <emilycodeee@gmail.com>
2022-01-18 18:18:36 +0800

    add:reaction-timer

---
+++ reaction-timer/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <h1>Welcome to Reaction Timmer</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="stopGame" />
+  <Result v-if="showResult" :reactionTime="reactionTime" />
+</template>
+
+<script>
+import Block from "./components/Block.vue";
+Block;
+import Result from "./components/Result.vue";
+
+export default {
+  name: "App",
+  components: { Block, Result },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      reactionTime: null,
+      showResult: false,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      this.showResult = false;
+    },
+    stopGame(time) {
+      this.reactionTime = time;
+      this.isPlaying = false;
+      this.showResult = true;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
+}
+</style>
---
+++ reaction-timer/src/components/Result.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction time - {{ reactionTime }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ["reactionTime"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.reactionTime < 250) {
+      this.rank = "Ninja Fingers";
+    } else if (this.reactionTime < 400) {
+      this.rank = "Rapid Reflexes";
+    } else {
+      this.rank = "snail pacs...";
+    }
+  },
+};
+</script>
+
+<style>
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
