ec5ffa6fe473ddd9a0cff1825aa8da4a00019574
Emily Chen <emilycodeee@gmail.com>
2022-01-20 09:47:53 +0800

    add : Joblist

---
+++ Joblist/src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="app">
+    <header>
+      <div class="order">
+        <button @click="handleOrder('title')">Order by title</button>
+        <button @click="handleOrder('salary')">Order by salary</button>
+        <button @click="handleOrder('location')">Order by location</button>
+      </div>
+    </header>
+    <Joblist :jobs="jobs" :order="order" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref, toRefs } from "vue";
+import Job from "./types/Job";
+import OrderTerm from "./types/OrderTerm";
+import Joblist from "./components/Joblist.vue";
+export default defineComponent({
+  name: "App",
+  components: { Joblist },
+  setup() {
+    const jobs = ref<Job[]>([
+      {
+        title: "farm worker",
+        location: "lon lon ranch",
+        salary: 30000,
+        id: "1",
+      },
+      {
+        title: "quarryman",
+        location: "death mountain",
+        salary: 40000,
+        id: "2",
+      },
+      {
+        title: "flute player",
+        location: "the lost woods",
+        salary: 35000,
+        id: "3",
+      },
+      { title: "fisherman", location: "lake hylia", salary: 21000, id: "4" },
+      {
+        title: "prison guard",
+        location: "gerudo valley",
+        salary: 32000,
+        id: "5",
+      },
+    ]);
+
+    const order = ref<OrderTerm>("title");
+
+    const handleOrder = (term: OrderTerm) => {
+      order.value = term;
+    };
+
+    return { jobs, order, handleOrder };
+  },
+});
+</script>
+
+<style>
+header {
+  text-align: center;
+}
+header .order {
+  margin-top: 20px;
+}
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
+</style>
